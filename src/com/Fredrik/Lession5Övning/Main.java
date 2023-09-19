e402ef815b374e7288d9079f7100ebbe52cd096b
FredrikEisermanWendt <fredrikeiserman@gmail.com>
2023-09-19 15:12:12 +0200

    Fråga 1

--- src/com/Fredrik/Lession5Övning/Main.java
+++ src/com/Fredrik/Lession5Övning/Main.java
@@ -1,9 +1,18 @@
 package com.Fredrik.Lession5Övning;
 
+import org.w3c.dom.ls.LSOutput;
+
 import java.util.Scanner;
 
 public class Main {
 
-    Scanner scan = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("What is your name?");
+        String name = scan.nextLine();
+        System.out.println("Player #1: " + name );
+
+    }
 
 }
