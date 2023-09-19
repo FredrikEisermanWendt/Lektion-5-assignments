a553262a7a362bf922fb72c554a878878e24b118
FredrikEisermanWendt <fredrikeiserman@gmail.com>
2023-09-19 15:16:35 +0200

    Fråga två

--- src/com/Fredrik/Lession5Övning/Main.java
+++ src/com/Fredrik/Lession5Övning/Main.java
@@ -7,12 +7,28 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+
+        // Öv 1
         Scanner scan = new Scanner(System.in);
 
         System.out.println("What is your name?");
         String name = scan.nextLine();
         System.out.println("Player #1: " + name );
 
+        // Öv 2
+        System.out.println("Give me a number");
+        int x = scan.nextInt();
+        for (int i = 0; i < x; i++){
+            System.out.println(i + 1);
+        }
+
+        scan.nextLine();
+
+
+
+
     }
+
+
 
 }
